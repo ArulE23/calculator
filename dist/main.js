a874d7dfa2a745fe1519549af0d04d941ab5ff53
Arul <220395395+ArulE23@users.noreply.github.com>
2026-09-26 14:13:19 +1000

    Add updateDisplay function and AC button event

--- dist/main.js
+++ dist/main.js
@@ -1,13 +1,21 @@
 let displayStr = "";
 const display = document.querySelector("#display");
+function updateDisplay() {
+    if (!display)
+        return;
+    display.textContent = displayStr;
+}
 const calcBtns = document.querySelectorAll(".row > button");
 for (const calcBtn of calcBtns) {
     calcBtn?.addEventListener("click", () => {
         displayStr += calcBtn.value;
-        if (!display)
-            return;
-        display.textContent = displayStr;
+        updateDisplay();
     });
 }
+const acBtn = document.querySelector("#ac");
+acBtn?.addEventListener("click", () => {
+    displayStr = "";
+    updateDisplay();
+});
 export {};
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 let displayStr: string = "";
-
 const display: HTMLDivElement | null = document.querySelector("#display");
+function updateDisplay() {
+  if (!display) return;
+  display.textContent = displayStr;
+}
+
 const calcBtns = document.querySelectorAll<HTMLButtonElement>(".row > button");
 for (const calcBtn of calcBtns) {
   calcBtn?.addEventListener("click", () => {
     displayStr += calcBtn.value;
-    if (!display) return;
-    display.textContent = displayStr;
+    updateDisplay();
   })
 }
+
+const acBtn: HTMLButtonElement | null = document.querySelector<HTMLButtonElement>("#ac");
+acBtn?.addEventListener("click", () => {
+  displayStr = "";
+  updateDisplay();
+})
